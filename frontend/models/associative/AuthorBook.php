00ad5e0ec542cc3e32d2f5f6c08253510a54bcac
Artem Rodin <artrodindev@gmail.com>
2019-11-18 22:53:03 +0300

    added model for relation between author and book's entities

---
+++ frontend/models/associative/AuthorBook.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace frontend\models\associative;
+
+use Yii;
+use frontend\models\author\Author;
+use frontend\models\book\Book;
+
+/**
+ * This is the model class for table "{{%author_book}}".
+ *
+ * @property int $id
+ * @property int $book_id
+ * @property int $author_id
+ *
+ * @property Author $author
+ * @property Books $book
+ */
+class AuthorBook extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%author_book}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['book_id', 'author_id'], 'required'],
+            [['book_id', 'author_id'], 'integer'],
+            [['author_id'], 'exist', 'skipOnError' => true, 'targetClass' => Author::className(), 'targetAttribute' => ['author_id' => 'id']],
+            [['book_id'], 'exist', 'skipOnError' => true, 'targetClass' => Book::className(), 'targetAttribute' => ['book_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(Author::className(), ['id' => 'author_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBook()
+    {
+        return $this->hasOne(Book::className(), ['id' => 'book_id']);
+    }
+}
